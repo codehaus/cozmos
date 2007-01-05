dd0590d8f5fba2b425de9cb5efb492717f0147cd
paul <paul@d9f3e375-e524-0410-ba9e-ac1a393e4a9c>
2007-01-05 04:05:35 +0000

    dead parameter - attempt to get around Word bugs

---
+++ trunk/src/test/java/com/thoughtworks/cozmos/sitemesh/CozmosTextFilterTestCase.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.cozmos.sitemesh;
+
+import junit.framework.TestCase;
+
+public class CozmosTextFilterTestCase extends TestCase {
+
+    public void testSomert() {
+        CozmosTextFilter textFilter = new CozmosTextFilter();
+
+        String before = ""; 
+
+        textFilter.filter();
+    }
+
+}
