ec69663f7042c156224c7b8942b37d7ac103ad69
paul <paul@d9f3e375-e524-0410-ba9e-ac1a393e4a9c>
2007-01-11 07:56:14 +0000

    servlet to proxy to mod_dav_svn  ..... start of

---
+++ src/main/java/com/thoughtworks/cozmos/WebDavProxyServlet.java
@@ -0,0 +1,91 @@
+package com.thoughtworks.cozmos;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletConfig;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.CharArrayWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.MalformedURLException;
+import java.util.Enumeration;
+
+public class WebDavProxyServlet extends HttpServlet {
+
+    private String targetURL;
+    private String newPageTemplate;
+    private String passwordForGet;
+    private String usernameForGet;
+    private String host;
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+        targetURL = servletConfig.getInitParameter("mod_dav_svn_url");
+        newPageTemplate = servletConfig.getInitParameter("new_page_template_file");
+        //usernameForGet = servletConfig.getInitParameter("username_for_get");
+        //passwordForGet = servletConfig.getInitParameter("password_for_get");
+        try {
+            host = new URL(targetURL).getHost();
+        } catch (MalformedURLException e) {
+            throw new ServletException(e);
+        }
+
+        // WTF is setDefault global ? 
+//        Authenticator.setDefault(new Authenticator() {
+//            protected PasswordAuthentication getPasswordAuthentication() {
+//                System.out.println("--> h " + getRequestingHost());
+//                if (getRequestingHost().equals(host)) {
+//                return new PasswordAuthentication(
+//                        usernameForGet, passwordForGet.toCharArray());
+//                } else {
+//                    return null;
+//                }
+//            }
+//        });
+        super.init(servletConfig);
+
+
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String path = req.getServletPath();
+        URL url = new URL(targetURL + path);
+
+        HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+
+        int rc = urlc.getResponseCode();
+
+        if (rc == HttpURLConnection.HTTP_NOT_FOUND) {
+            url = new URL(targetURL + newPageTemplate);
+            urlc = (HttpURLConnection) url.openConnection();
+            rc = urlc.getResponseCode();
+        }
+
+        if (rc == HttpURLConnection.HTTP_OK) {
+            String mimeType = getServletContext().getMimeType(path);
+            resp.setContentType(mimeType);
+            InputStream is = urlc.getInputStream();
+            OutputStream os = resp.getOutputStream();
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = is.read(buffer)) >= 0) {
+                os.write(buffer, 0, length);
+            }
+        } else {
+            resp.sendError(rc);
+        }
+
+    }
+
+}
--- src/main/resources/decorators/site.jsp
+++ src/main/resources/decorators/site.jsp
@@ -5,7 +5,122 @@
 <head>
   <title><decorator:title/></title>
   <decorator:head/>
-  <link rel="stylesheet" type="text/css" href="<%= request.getContextPath() %>/style/style.css" />
+  <link rel="stylesheet" type="text/css" href="<%= request.getContextPath() %>/style/styleCss.jsp" />
+    <style type="text/css">
+body {
+	background: #FFF url('<%= request.getContextPath() %>/style/images/back8.jpg') no-repeat top left fixed;
+	margin: 0 0 20px 0;
+	padding: 0;
+	font-family: Verdana, sans-serif;
+	font-size: 0.95em;
+	color: #000;
+	line-height: 1.5em;
+}
+
+#header {
+ width: 80%;
+ background:transparent;
+ margin: 0 auto;
+ padding: 0;
+ height: 101px;
+
+}
+
+#header h1 {
+	padding: 30px 0;
+	color: #999;
+	font-size: 1.4em;
+	letter-spacing: 3px;
+	margin: 0;
+	text-align: right;
+}
+
+#nav {
+	position: absolute;
+	top: 85px;
+	left: 10%;
+	width: auto;
+	padding: 0;
+	margin: 0 auto;
+	font: bold 11px Verdana, sans-serif;
+	height: 25px;
+
+
+}
+
+#nav li {
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	display: inline;
+}
+
+#nav li a {
+	padding: 3px;
+	margin: 0;
+	border: 1px solid #B78E96;
+	border-bottom: none;
+	background: transparent url(<%= request.getContextPath() %>/style/images/trans6.gif) repeat;
+	text-decoration: none;
+
+}
+
+#nav li a:link { color: #000; }
+#nav li a:visited { color: #fff; }
+
+#nav li a:hover {
+	color: #fff;
+	background: transparent url(<%= request.getContextPath() %>/style/images/trans7.gif) repeat;
+}
+
+#nav li a#current {
+	background: transparent url(<%= request.getContextPath() %>/style/images/trans7.gif) repeat;
+	border-bottom: 1px solid #999;
+}
+
+#container {
+	background: transparent url('<%= request.getContextPath() %>/style/images/trans5.gif') repeat;
+	width: 80%;
+	margin: 0 auto;
+	padding: 0;
+}
+
+#content {
+	margin: 0;
+	padding: 40px;
+
+}
+
+#footer {
+	background: transparent;
+	padding: 5px 10px 2px 0;
+	text-align: right;
+	font-size: 0.75em;
+	clear: both;
+	line-height: 1.15em;
+}
+
+a, a:visited {
+	background: transparent;
+	color: #333;
+	border-bottom: 1px solid #333;
+	text-decoration: none;
+	font-weight: bold;
+}
+
+a:hover, a:active {
+	background: transparent;
+	color: #666;
+}
+
+h2 {
+	color: #666;
+	border-bottom: 1px solid #666;
+	margin: 0;
+	padding: 10px 0 1px 0;
+	font-size: 1.2em;
+}
+    </style>
   </head>
 <body>
 <div id="header">
