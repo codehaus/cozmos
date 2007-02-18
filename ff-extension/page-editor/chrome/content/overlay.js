96fad94b0e40682668fa0b6b2db7f82f4b5aa6f3
paul <paul@d9f3e375-e524-0410-ba9e-ac1a393e4a9c>
2007-02-18 22:07:53 +0000

    start of ff plugin

---
+++ ff-extension/page-editor/chrome/content/overlay.js
@@ -0,0 +1,8 @@
+/* oncommand event */
+function edit_page()
+  {
+    // set the source 
+    var src = gContextMenu.target.src;
+    // set the destination filename and open save dialog
+    alert("hello!" + src)
+  }
