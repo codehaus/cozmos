83474611a95c1b323c3d1c6470c3d96924ad1588
paul <paul@d9f3e375-e524-0410-ba9e-ac1a393e4a9c>
2007-02-19 02:29:22 +0000

    add editMode logic

--- ff-extension/src/chrome/content/edit_page.js
+++ ff-extension/src/chrome/content/edit_page.js
@@ -1,17 +1,25 @@
 /* oncommand event */
 function edit_page() {
 
-    var src = getWebNavigation().currentURI.spec;
+    var url = getWebNavigation().currentURI.spec;
+
+    if (url.substr(0,4) == "http") {
+        if (url.indexOf("?") > 0) {
+            url = url + "&editMode=true";
+        } else {
+            url = url + "?editMode=true";
+        }
+    }
 
     // window.alert("hello! 2 " + src);
 
     var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 
     file.initWithPath("/Applications/Macromedia Dreamweaver 8/Dreamweaver 8/Contents/MacOS/Dreamweaver");
-    var args = new Array(src);
+    var args = new Array(url);
     if (!file.exists()) {
         file.initWithPath("/Applications/SeaMonkey.app/Contents/MacOS/seamonkey");
-        args = new Array("-editor", src);
+        args = new Array("-editor", url);
     }
 
     var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
