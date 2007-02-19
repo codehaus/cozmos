901af973880626c4ffefc2a9eb846bec5c848ecd
paul <paul@d9f3e375-e524-0410-ba9e-ac1a393e4a9c>
2007-02-19 01:11:28 +0000

    done for mac and seamonkey

--- ff-extension/src/chrome/content/edit_page.js
+++ ff-extension/src/chrome/content/edit_page.js
@@ -1,24 +1,13 @@
 /* oncommand event */
 function edit_page()
   {
-    // set the source 
-    var src = window.location.href;
-    // set the destination filename and open save dialog
 
-    window.alert("hello!" + src)
+    var src = getWebNavigation().currentURI.spec;
+
+    // window.alert("hello! 2 " + src);
 
     var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 
-    /*FIXME isWritable()
-    // Check the file is writable
-    file.initWithPath(dest);
-    if (!file.isWritable())
-      {
-        alert(gStringBundle.GetStringFromName("mozvertErrorDest"));
-        return;
-      }*/
-
-    // Set the path to convert and check it
     file.initWithPath("/Applications/SeaMonkey.app/Contents/MacOS/seamonkey");      
 
     var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
@@ -27,6 +16,5 @@ function edit_page()
     var args = new Array("-editor", src);
 
     process.run(false, args, args.length);
-
 
   }
