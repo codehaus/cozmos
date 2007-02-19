09c0f1a5517200920be719cd0cd672adf7dcfe74
paul <paul@d9f3e375-e524-0410-ba9e-ac1a393e4a9c>
2007-02-19 00:22:36 +0000

    closer

--- ff-extension/src/chrome/content/edit_page.js
+++ ff-extension/src/chrome/content/edit_page.js
@@ -2,7 +2,31 @@
 function edit_page()
   {
     // set the source 
-    var src = gContextMenu.target.src;
+    var src = window.location.href;
     // set the destination filename and open save dialog
-    alert("hello!" + src)
+
+    window.alert("hello!" + src)
+
+    var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+
+    /*FIXME isWritable()
+    // Check the file is writable
+    file.initWithPath(dest);
+    if (!file.isWritable())
+      {
+        alert(gStringBundle.GetStringFromName("mozvertErrorDest"));
+        return;
+      }*/
+
+    // Set the path to convert and check it
+    file.initWithPath("/Applications/SeaMonkey.app/Contents/MacOS/seamonkey");      
+
+    var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
+    process.init(file);
+
+    var args = new Array("-editor", src);
+
+    process.run(false, args, args.length);
+
+
   }
